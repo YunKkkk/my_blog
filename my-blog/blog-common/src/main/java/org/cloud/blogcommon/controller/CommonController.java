665a76b55bb9035dc59bc183a46931213a700280
YunKkkk <dyh741657141@gmail.com>
2024-09-27 09:37:03 +0800

    Initial commit

---
+++ my-blog/blog-common/src/main/java/org/cloud/blogcommon/controller/CommonController.java
@@ -0,0 +1,4 @@
+package org.cloud.blogcommon.controller;
+
+public class CommonController {
+}
---
+++ my-blog/blog-common/src/test/java/org/cloud/blogcommon/BlogCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cloud.blogcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
